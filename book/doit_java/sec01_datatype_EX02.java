07a6a888135cb29856f6f9576717852adaad4a8b
Jinhong Park <deepredpark@gmail.com>
2021-11-17 23:17:48 +0900

    Create sec01_datatype_EX02.java

---
+++ book/doit_java/sec01_datatype_EX02.java
@@ -0,0 +1,19 @@
+package sec01_datatype.EX02_NamingVariableAndConstant;
+
+public class NamingVariableAndConstant {
+	public static void main(String[] args) {
+		// 변수
+		boolean aBcD;
+		byte 가나다; // 한글 네이밍 권장X
+		short _abcd;
+		char $ab_cd;
+		long abcd3;
+		double main;
+		String myClassName;
+		int ABC; // 변수 전부 대문자 권장X
+		// 상수
+		final double PI;
+		final int MY_DATA;
+		final float myData; // 변수명과 헷갈리니 권장X
+	}
+}
